45419f3440cb2b174a2fa36d6d381a3c5cb7cbe4
uly55e5 <post@davidrauh.de>
2022-08-02 17:28:47 +0200

    small changes in build

--- mzmlReader/pWizMSData.go
+++ mzmlReader/pWizMSData.go
@@ -1,10 +1,11 @@
 package mzmlReader
 
+import "C"
+
 //#cgo CFLAGS: -I../pwiz-wrapper
 //#cgo LDFLAGS: -L../pwiz-wrapper/lib -lpwiz_wrapper -Wl,-rpath=../pwiz-wrapper/lib -lstdc++ -lpwiz_all -lm -ldl
 //#include "cpwiz.h"
 //#define _GLIBCXX_USE_CXX11_ABI 0
-import "C"
 
 type MSData struct {
 	msData C.MSDataFile
--- mzmlReader/pWizMSData_test.go
+++ mzmlReader/pWizMSData_test.go
@@ -12,7 +12,7 @@ func TestOpenMSData(t *testing.T) {
 		name string
 		args args
 	}{
-		{"Read Example", args{"data/examples/small.pwiz.1.1.mzML"}},
+		{"Read Example", args{"../data/examples/small.pwiz.1.1.mzML"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
